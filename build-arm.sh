c93787cea46cb69f532cac4814e062b4292375bd
Akash Yadav <itsaky01@gmail.com>
2023-03-19 14:15:14 +0530

    feat: update build scripts
    Signed-off-by: Akash Yadav <itsaky01@gmail.com>

--- build-arm.sh
+++ build-arm.sh
@@ -2,10 +2,12 @@
 
 set -eu
 
+packages=$(<packages.txt)
+
 for pck in $(<packages.txt); do
   if [ -f "/data/data/.built-packages/$pck" ]; then
     echo "Skipping $pck as it is already built"
     continue
   fi
-  ./build-package.sh -a aarch64 -o output $pck
+  ./build-package.sh -a arm -o output-arm $pck
 done
